49debf69dd2e57d5209c4c02c631bc0d1c5c813d
codaryf <codaryf@gmail.com>
2025-03-20 13:02:08 +0100

    MidiUSB init basic functions for midi messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,41 @@
 #include <Arduino.h>
+#include <MIDIUSB.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-
-  pinMode(23, OUTPUT);
- digitalWrite(23, HIGH);
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+void noteOn(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(23, !digitalRead(23));
-  delay(1000);
+void noteOff(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+void controlChange(byte channel, byte control, byte value)
+{
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
+void setup()
+{
+
+}
+
+void loop()
+{
+noteOn(0, 48, 127);
+delay(500);
+noteOn(0, 48, 0);
+delay(500);
 }
